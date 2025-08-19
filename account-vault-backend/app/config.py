a313612efadfb41c701464fbc1cd76f89d7d8885
TronJG <109089333+TronJG@users.noreply.github.com>
2025-08-20 01:07:28 +0700

    Update config.py

--- account-vault-backend/app/config.py
+++ account-vault-backend/app/config.py
@@ -8,6 +8,9 @@ JWT_ALG = os.getenv("JWT_ALG", "HS256")
 JWT_EXPIRE_MIN = int(os.getenv("JWT_EXPIRE_MIN", "43200"))
 JWT_EXPIRE = timedelta(minutes=JWT_EXPIRE_MIN)
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # Render sẽ lấy từ Environment Variables
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg://neondb_owner:PASS@ep-rapid-moon-a12f8rmt-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+)
 
 APP_SECRET = os.getenv("APP_SECRET", "appsecret")
